420829f7df6101377ebf23cf56e695bc92d5f6a0
ruben <ruben.martinez@t-evolvers.com>
2025-02-20 14:39:05 -0500

    fix afterAll and test in package.json

--- tests/hooks/hooks.js
+++ tests/hooks/hooks.js
@@ -1,10 +1,13 @@
 import { chromium, firefox } from '@playwright/test';
 import { createBdd } from 'playwright-bdd';
+import fs from 'fs';
+import path from 'path';
 
 
 const { Before, After, BeforeAll, AfterAll,  } = createBdd();
 
 Before(async({page})=>{    
+    console.log("🔥 Iniciando la prueba...");
     await page.goto('https://ecommerce-playground.lambdatest.io/')    
 });
 
@@ -13,12 +16,24 @@ After(async()=>{
     console.log("❌ Cerrando la página después de cada escenario...");
 });
 
-BeforeAll(async({browser})=>{
-    console.log("🔥 Iniciando la prueba...");
+BeforeAll(async({browser})=>{    
     const page = chromium.launch();
 });
 
-AfterAll(async()=>{
-    console.log("This is a AfterAll.")
-    console.log("🧹 Finalizando pruebas y limpiando recursos...");  
+AfterAll(async () => {
+    console.log("🧹 Eliminando la carpeta .features-gen...");
+
+    const generatedDir = path.join(__dirname, '../../.features-gen');
+
+    if (fs.existsSync(generatedDir)) {
+        fs.rm(generatedDir, { recursive: true, force: true }, (err) => {
+            if (err) {
+                console.error("❌ Error al eliminar la carpeta .features-gen:", err);
+            } else {
+                console.log("✅ Carpeta .features-gen eliminada correctamente.");
+            }
+        });
+    } else {
+        console.log("📂 No se encontró la carpeta .features-gen, omitiendo limpieza.");
+    }
 });
