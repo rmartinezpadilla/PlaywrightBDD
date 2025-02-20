158da424b94b93056dec03791c4a779ff49aac2a
ruben <ruben.martinez@t-evolvers.com>
2025-02-20 14:53:51 -0500

    add new feature

--- playwright.config.js
+++ playwright.config.js
@@ -17,10 +17,10 @@ const testDir = defineBddConfig({
 
 export default defineConfig({
   testDir,
-  fullyParallel: true,
-  forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
+  fullyParallel: false,
+  // forbidOnly: !!process.env.CI,
+  // retries: process.env.CI ? 2 : 0,
+  // workers: process.env.CI ? 1 : undefined,
   reporter: 'html',
   use: {
     trace: 'on-first-retry',
